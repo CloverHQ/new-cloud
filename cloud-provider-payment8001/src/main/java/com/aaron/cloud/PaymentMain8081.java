2698f5f0bc5b33beae6670f9654419a0dff2414b
clover <775856746@qq,com>
2020-03-21 11:31:48 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/aaron/cloud/PaymentMain8081.java
@@ -0,0 +1,16 @@
+package com.aaron.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuwenchao
+ * @create 2020-03-21 11:21
+ **/
+@SpringBootApplication
+public class PaymentMain8081 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8081.class, args);
+    }
+}
