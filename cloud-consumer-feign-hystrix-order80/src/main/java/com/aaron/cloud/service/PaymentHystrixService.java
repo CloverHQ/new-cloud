da11d4201b26d73bf3019d38c88e9caba6d96ea4
clover <775856746@qq,com>
2020-06-13 21:21:12 +0800

    集成hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/aaron/cloud/service/PaymentHystrixService.java
@@ -0,0 +1,20 @@
+package com.aaron.cloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author liuwenchao
+ * @create 2020-06-13 21:16
+ **/
+@FeignClient("CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+
+}
