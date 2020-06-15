295f93c6e90c20456fd219797086ff6505e04bbb
aaron <775856746@qq.com>
2020-06-15 22:32:17 +0800

    hystrix  dashboard

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/aaron/cloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/aaron/cloud/OrderHystrixMain80.java
@@ -1,9 +1,12 @@
 package com.aaron.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author liuwenchao
@@ -15,5 +18,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class OrderHystrixMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean() {
+        HystrixMetricsStreamServlet servlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> bean = new ServletRegistrationBean<>(servlet);
+        bean.setLoadOnStartup(1);
+        bean.addUrlMappings("/hystrix.stream");
+        bean.setName("HystrixMetricsStreamServlet");
+        return bean;
+
     }
 }
