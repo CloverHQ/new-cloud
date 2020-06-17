a47c21166a5bac931ad259c76b3ba2e0d2cf437b
aaron <775856746@qq.com>
2020-06-17 23:29:12 +0800

    Gateway

--- cloud-gateway-gateway9527/src/main/java/con/aaron/cloud/GatewayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/con/aaron/cloud/GatewayMain9527.java
@@ -1,18 +1,14 @@
-package com.aaron.cloud;
+package con.aaron.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * @author liuwenchao
- * @create 2020-06-13 21:01
- **/
-@SpringBootApplication
 @EnableEurekaClient
-public class PaymentHystrixMain8001 {
+@SpringBootApplication
+public class GatewayMain9527 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentHystrixMain8001.class, args);
+        SpringApplication.run(GatewayMain9527.class, args);
     }
 }
